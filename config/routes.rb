4ff01d9ce183e5195984ef9951750016f3660df2
Robin Unger <robin.e.unger@gmail.com>
2019-10-08 19:36:01 -0500

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get       "/signup",        to: "users#new"
   post      "/signup",        to: "users#create"
-  resources :users, only: [:new, :create, :show]
+  get       "login",          to: "sessions#new"
+  post      "login",          to: "sessions#create"
+  delete    "logout",         to: "sessions#destroy"
+  resources :users, only: [:create, :new, :show]
+  resources :sessions, only: [:create, :new, :destroy]
 end
