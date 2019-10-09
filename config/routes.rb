41c5ad8b6e8e6d77df48ca06cc7693943ec48fbd
Robin Unger <robin.e.unger@gmail.com>
2019-10-08 19:07:30 -0500

    Generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'new/create'
+  get 'new/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
