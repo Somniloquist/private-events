ac832e684bb021fc6f1771a4693d9caa712bb3e8
Robin Unger <robin.e.unger@gmail.com>
2019-10-09 17:22:59 -0500

    Add basic sessions login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+    current_user = user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    @current_user = nil
+    session.delete :user_id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session[:id])
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
 end
