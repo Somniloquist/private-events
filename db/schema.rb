e339fa6c1b437bd3ab2db69f7fa65846266320a7
Robin Unger <robin.e.unger@gmail.com>
2019-10-09 20:04:18 -0500

    Add event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_000433) do
+ActiveRecord::Schema.define(version: 2019_10_10_005759) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
