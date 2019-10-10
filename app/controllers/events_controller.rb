6589db8834d6f74cdcaff0581a47fa7334b7e6f8
Robin Unger <robin.e.unger@gmail.com>
2019-10-09 21:58:38 -0500

    Add event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,23 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def show
   end
 end
+
+private
+  def event_params
+    params.require(:event).permit(:title, :date, :location, :description)
+  end
