7295b9ae326ea35a4a4ff468b572149259c32faa
Robin Unger <robin.e.unger@gmail.com>
2019-10-09 20:44:42 -0500

    Add event/user associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_005759) do
+ActiveRecord::Schema.define(version: 2019_10_10_010832) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "date"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_10_10_005759) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
