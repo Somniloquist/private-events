2e9290a8d604ee30d946d5bfb0f6efdf1b133d0d
Robin Unger <robin.e.unger@gmail.com>
2019-10-12 18:29:33 -0500

    Update migration for use with postgreSQL

--- db/migrate/20191010010832_add_creator_to_event.rb
+++ db/migrate/20191010010832_add_creator_to_event.rb
@@ -1,5 +1,5 @@
 class AddCreatorToEvent < ActiveRecord::Migration[5.2]
   def change
-    add_reference :events, :creator, foreign_key: true
+    add_reference :events, :creator, foreign_key: { to_table: :users }
   end
 end
